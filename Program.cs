286cb14e3130910d2108d0dd48f1c2b3db000f37
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-11 23:09:44 +0800

    Initialized

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int Compare (int a,int b)
+{
+    int max;
+
+    if (a > b)
+        max = a;
+    else
+        max = b
+    
+    return max;
+}
+
+a = 7;
+b = 2;
+
+Console.Writeline(Compare(a,b));
+
